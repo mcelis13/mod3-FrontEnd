b7252dbca468e19896ab3d5b307cf15c24c216a0
Madeline Celis <celiscb@vcu.edu>
2018-10-03 20:32:49 -0400

    updates to frontEnd

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   const endPoint = 'http://localhost:3000/api/v1/messages';
   const myWebSocket = new WebSocket('ws://localhost:3000/cable');
+  const userInput = document.querySelector("#new_message");
+  const sendButton = document.querySelector("#send");
+  const messButton = document.querySelector('#mess');
 
   myWebSocket.onopen = (event) => {
   const subscribeUserMessage = {"command": "subscribe", "identifier": "{\"channel\":\"ChatChannel\"}"}
-  myWebSocket.send(JSON.stringify(subscribeUserMessage));
-  console.log("WebSocket is open now.");
+    myWebSocket.send(JSON.stringify(subscribeUserMessage));
+    sendButton.addEventListener('click', function(event){
+      event.preventDefault();
+      let message = {
+        "command":"message",
+        "identifier":"{\"channel\":\"ChatChannel\"}",
+        "data":`{
+          \"action\":\"onChat\",
+          \"content\":\"${userInput.value}\"
+        }`
+      };
+      console.log(message);
+      myWebSocket.send(JSON.stringify(message));
+    })
   };
-
-  let readyState = myWebSocket.readyState;
-  console.log(readyState);
 
   fetch(endPoint)
     .then(resp => resp.json())
@@ -26,4 +38,4 @@ document.addEventListener('DOMContentLoaded', () => {
       messageBoard.append(p);
     }
 
-  });
+   });
